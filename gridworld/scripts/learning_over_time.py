517e7982092f775d6090ef7352813284c938484a
Jasmine Noack <jnoack@understood.org>
2025-05-07 00:44:03 -0400

    fix rendering

--- gridworld/scripts/learning_over_time.py
+++ gridworld/scripts/learning_over_time.py
@@ -6,7 +6,7 @@ from gridworld.agents.generic_agent import Agent
 from gridworld.agents.q_learning_agent import QLearningAgent
 from gridworld.components.grid_environment import GridWorldEnv, VisitCounter
 from rich.console import Console
-from gridworld.utils import render_heatmap
+from gridworld.utils import render_directional_heatmap_for_q_table, render_heatmap
 
 from gridworld.runner import Runner
 
@@ -32,12 +32,11 @@ def log(*message: list[str]):
 
 def run_test(env: GridWorldEnv, agent: Agent, iteration: int, render: bool = False):
     runner = Runner(env, agent)
-    results = runner.run_episodes(10, render=False)
+    results = runner.run_episodes(3, render=False)
     analysis = runner.analyze_results(results)
 
     visit_counts = [result["visit_counts"] for result in results]
     avg_visit_counts = VisitCounter.avg(*visit_counts)
-    total_visit_counts = reduce(lambda x, y: x + y, visit_counts, VisitCounter())
 
     log(f"Analysis of 10 episodes with {agent.__class__.__name__} - {iteration}:")
     log("Average Reward:", analysis["reward"]["average"])
@@ -48,28 +47,21 @@ def run_test(env: GridWorldEnv, agent: Agent, iteration: int, render: bool = Fal
     log("Min Steps:", analysis["steps"]["min"])
     log("Reached Goal Count:", analysis["reached_goal"]["count"])
     log("Reached Goal Percentage:", analysis["reached_goal"]["count"] / 10 * 100)
-    render_heatmap(
+    render_directional_heatmap_for_q_table(
         visit_counts=avg_visit_counts,
         rows=env.rows,
         cols=env.cols,
-        stat=f"Avg Visit Count ({agent.__class__.__name__})",
+        q_table=agent.q_table,
+        stat=f"Avg Directional Visit Count ({agent.__class__.__name__})",
         folder=folder,
         scale_max=5,
-        filename=f"avg_visit_count_{iteration}",
+        filename=f"avg_directional_visit_count_{iteration}",
     )
-    render_heatmap(
-        visit_counts=total_visit_counts,
-        rows=env.rows,
-        cols=env.cols,
-        stat=f"Total Visit Count ({agent.__class__.__name__})",
-        folder=folder,
-        scale_max=25,
-        filename=f"total_visit_count_{iteration}",
-    )
+
     log()
 
 
 env = GridWorldEnv()
 learning_agent = QLearningAgent()
-for i in range(5):
+for i in range(15):
     run_test(env, learning_agent, iteration=i + 1, render=False)
--- gridworld/utils.py
+++ gridworld/utils.py
@@ -1,3 +1,4 @@
+from re import L
 from typing import NamedTuple, TypedDict
 
 from matplotlib import pyplot as plt
@@ -44,6 +45,109 @@ def render_heatmap(
 
     name = f"Gridworld {stat} Heatmap"
     plt.imshow(numpy_map, cmap="hot", interpolation="nearest", vmax=scale_max)
+    plt.colorbar(label=stat)
+    plt.title(name)
+    plt.show(block=False)
+    plt.pause(2)
+    filename = (
+        filename
+        or f'{name.lower().replace(" ", "_").replace("(", "_").replace(")", "_")}'
+    )
+    plt.savefig(f"{folder}/{filename}.png")
+    plt.close()
+
+
+def render_directional_heatmap_for_q_table(
+    *,
+    visit_counts: dict[tuple[int, int], int],
+    rows: int,
+    cols: int,
+    q_table: dict[tuple[int, int], dict[str, float]],
+    stat="Favorite Direction",
+    folder: str = "output",
+    filename: str | None = None,
+    scale_max: int = 20,
+) -> None:
+    all_confidences = [
+        max(actions.values()) - sorted(actions.values())[-2]
+        for actions in q_table.values()
+        if len(set(actions.values())) > 1
+    ]
+    max_conf = max(all_confidences) if all_confidences else 1.0
+
+    numpy_map = np.zeros((rows, cols))
+    for (x, y), count in visit_counts.items():
+        numpy_map[x, y] = count
+
+    name = f"Gridworld {stat} Heatmap"
+    plt.imshow(numpy_map, cmap="hot", interpolation="nearest", vmax=scale_max)
+    for (x, y), actions in q_table.items():
+        q_values = sorted(list(actions.values()))
+        max_action_value = q_values[-1]
+        second_highest_action_value = q_values[-2]
+        confidence = (max_action_value - second_highest_action_value) / max_conf
+
+        if confidence == 0:
+            continue
+        scale = max(0.2, min(confidence, 1.0))
+
+        max_actions = [
+            action for action, value in actions.items() if value == max_action_value
+        ]
+
+        if max_action_value < 0:
+            color = "red"
+        elif max_action_value > 0:
+            color = "green"
+        else:
+            color = "yellow"
+
+        if len(max_actions) > 1:
+            continue
+        if UP in max_actions:
+            plt.arrow(
+                y,
+                x,
+                0,
+                -scale,
+                head_width=0.1 * scale,
+                head_length=0.2 * scale,
+                fc=color,
+                ec=color,
+            )
+        elif DOWN in max_actions:
+            plt.arrow(
+                y,
+                x,
+                0,
+                scale,
+                head_width=0.1 * scale,
+                head_length=0.2 * scale,
+                fc=color,
+                ec=color,
+            )
+        elif LEFT in max_actions:
+            plt.arrow(
+                y,
+                x,
+                -scale,
+                0,
+                head_width=0.1 * scale,
+                head_length=0.2 * scale,
+                fc=color,
+                ec=color,
+            )
+        elif RIGHT in max_actions:
+            plt.arrow(
+                y,
+                x,
+                scale,
+                0,
+                head_width=0.1 * scale,
+                head_length=0.2 * scale,
+                fc=color,
+                ec=color,
+            )
     plt.colorbar(label=stat)
     plt.title(name)
     plt.show(block=False)
