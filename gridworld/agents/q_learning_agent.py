a342b5583e43dab962377492abffa5179959a4be
Jasmine Noack <jnoack@understood.org>
2025-05-06 23:55:26 -0400

    add reinforcement learning agent

--- gridworld/agents/q_learning_agent.py
+++ gridworld/agents/q_learning_agent.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
+from email.policy import default
 from random import Random
 from gridworld.agents.generic_agent import Agent
-from gridworld.utils import SIMPLE_ACTIONS
+from gridworld.utils import SIMPLE_ACTIONS, Step
 
 
 class QLearningAgent(Agent):
     def __init__(self, *, rng: Random | None = None):
         self.actions = SIMPLE_ACTIONS
-        self.q_table = {action: 0 for action in self.actions}
+        self.q_table = defaultdict(lambda: {action: 0 for action in self.actions})
         # exploration rate
         self.epsilon = 0.1
         # learning rate (how fast Q-values update)
@@ -15,3 +16,44 @@ class QLearningAgent(Agent):
         # discount factor (importance of future reward)
         self.gamma = 0.9
         self.rng = rng or Random()
+        self.reset()
+
+    def act(self, state):
+        """
+        using epsilon sometimes make a random choice
+
+        otherwise choose the option that you know from history is one
+        of the best options
+        """
+        if self.rng.random() < self.epsilon:
+            return self.rng.choice(self.actions)
+
+        q_values = self.q_table[state]
+        max_q_value = max(q_values.values())
+
+        best_actions = [
+            action for action, q_value in q_values.items() if q_value == max_q_value
+        ]
+        return self.rng.choice(best_actions)
+
+    def reset(self):
+        pass
+
+    def observe(self, step: Step):
+        state = step.start
+        next_state = step.new_state
+        action = step.action
+        reward = step.reward
+        done = step.done
+
+        current_q = self.q_table[state][action]
+
+        if done:
+            best_future_q = 0
+        else:
+            best_future_q = max(self.q_table[next_state].values())
+
+        updated_q = current_q + self.alpha * (
+            reward + self.gamma * best_future_q - current_q
+        )
+        self.q_table[state][action] = updated_q
--- gridworld/agents/tests/test_q_learning_agent.py
+++ gridworld/agents/tests/test_q_learning_agent.py
@@ -1,1 +1,102 @@
+import random
 from gridworld.agents.q_learning_agent import QLearningAgent
+from gridworld.utils import LEFT, RIGHT, UP, DOWN, Step
+
+
+class TestAct:
+    def test_returns_random_action_when_epsilon(self, mocker):
+        rng = mocker.Mock()
+        rng.random.return_value = 0.05
+        agent = QLearningAgent(rng=rng)
+        agent.q_table = {(0, 0): {UP: 0, DOWN: 1, LEFT: 1, RIGHT: 0}}
+        agent.act((0, 0))
+        rng.choice.assert_called_once_with(agent.actions)
+
+    def test_returns_best_action_when_not_epsilon(self, mocker):
+        rng = mocker.Mock()
+        rng.random.return_value = 0.2
+        agent = QLearningAgent(rng=rng)
+        agent.q_table = {(0, 0): {UP: 0, DOWN: 1, LEFT: 1, RIGHT: 0}}
+        agent.act((0, 0))
+        rng.choice.assert_called_once_with([DOWN, LEFT])
+
+    def test_returns_an_action_with_max_q_value(self, mocker):
+        rng = random.Random(42)
+        agent = QLearningAgent(rng=rng)
+        agent.q_table = {(0, 0): {UP: 1, DOWN: 0, LEFT: 0, RIGHT: 0}}
+        action = agent.act((0, 0))
+        assert action == UP
+
+
+class TestReset:
+    def test_do_not_reset_q_table(self):
+        agent = QLearningAgent()
+        old_table = {(0, 0): {UP: 1, DOWN: 0, LEFT: 0, RIGHT: 0}}
+        agent.q_table = old_table
+        agent.reset()
+        assert isinstance(agent.q_table, dict)
+        assert agent.q_table == old_table
+
+
+class TestObserve:
+    def test_initial_q_table_update(self):
+        agent = QLearningAgent()
+        agent.q_table[(0, 0)] = {UP: 0, DOWN: 0, LEFT: 0, RIGHT: 0}
+        step = Step(
+            start=(0, 0),
+            action=UP,
+            reward=-10,
+            new_state=(0, 0),
+            done=False,
+        )
+        agent.observe(step)
+        assert agent.q_table == {
+            (0, 0): {UP: -1, DOWN: 0, LEFT: 0, RIGHT: 0},
+        }
+
+    def test_handles_more_information_if_it_knows_something_already(self):
+        agent = QLearningAgent()
+        agent.q_table[(0, 0)] = {UP: -6, DOWN: 0, LEFT: 0, RIGHT: 0}
+        step = Step(
+            start=(0, 0),
+            action=UP,
+            reward=-10,
+            new_state=(0, 1),
+            done=False,
+        )
+        agent.observe(step)
+        assert agent.q_table == {
+            (0, 0): {UP: -6.4, DOWN: 0, LEFT: 0, RIGHT: 0},
+            (0, 1): {UP: 0, DOWN: 0, LEFT: 0, RIGHT: 0},
+        }
+
+    def test_updates_if_already_at_score(self):
+        agent = QLearningAgent()
+        agent.q_table[(0, 0)] = {UP: -10, DOWN: 0, LEFT: 0, RIGHT: 0}
+        step = Step(
+            start=(0, 0),
+            action=UP,
+            reward=-10,
+            new_state=(0, 0),
+            done=True,
+        )
+        agent.observe(step)
+        assert agent.q_table == {
+            (0, 0): {UP: -10, DOWN: 0, LEFT: 0, RIGHT: 0},
+        }
+
+    def test_updates_for_next_location_score(self):
+        agent = QLearningAgent()
+        agent.q_table[(1, 0)] = {UP: 0, DOWN: 0, LEFT: 10, RIGHT: 0}
+        step = Step(
+            start=(0, 0),
+            action=DOWN,
+            reward=-1,
+            new_state=(1, 0),
+            done=False,
+        )
+        agent.observe(step)
+        assert agent.q_table == {
+            (0, 0): {UP: 0, DOWN: 0.8, LEFT: 0, RIGHT: 0},
+            (1, 0): {UP: 0, DOWN: 0, LEFT: 10, RIGHT: 0},
+        }
