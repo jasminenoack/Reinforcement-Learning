ef3cdffcb8e71be920a00cade47ef8fae7dc5136
Jasmine Noack <jasminenoack@gmail.com>
2025-05-12 19:38:49 -0400

    adds create board

---
+++ queens/tests/utils/test_build_board_array.py
@@ -0,0 +1,30 @@
+from queens.utils import build_board_array
+
+
+class TestBuildBoardArray:
+    def test_builds_empty_board(self):
+        board = build_board_array([])
+        assert board.tolist() == [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+    def test_places_queens_on_board(self):
+        queens = [(0, 0), (1, 2), (2, 4)]
+        board = build_board_array(queens)
+        assert board.tolist() == [
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
---
+++ queens/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def build_board_array(queens: list[tuple[int, int]]):
+    board = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+    for row in queens:
+        board[row[0]][row[1]] = 1
+    return np.array(board)
